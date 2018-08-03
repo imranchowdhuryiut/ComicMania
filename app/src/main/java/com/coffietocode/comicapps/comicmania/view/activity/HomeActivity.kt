a91ebbe8c13d854e707f9af153433b509167345c
Imran Chowdhury <imran.singularitybd@gmail.com>
2018-08-03 22:32:30 +0600

    Home Screen Created

--- app/src/main/java/com/coffietocode/comicapps/comicmania/view/activity/HomeActivity.kt
+++ app/src/main/java/com/coffietocode/comicapps/comicmania/view/activity/HomeActivity.kt
@@ -1,17 +1,57 @@
 package com.coffietocode.comicapps.comicmania.view.activity
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.coffietocode.comicapps.comicmania.R
-
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.toolbar.*
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+
+        setupNavigationDrawer()
     }
 
+    private fun setupNavigationDrawer() {
+        val drawerToggle = ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.action_drawer_open,
+                R.string.action_drawer_close)
+
+        drawer_layout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        navigation_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            val itemId = item.itemId
+            when (itemId) {
+                R.id.change_theme -> {
+                    return false
+                }
+            }
+        }
+        return false
+    }
 }
